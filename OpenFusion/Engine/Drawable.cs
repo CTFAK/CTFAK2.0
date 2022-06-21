9e104304a55e7bb9de9171be3f5c3b46565793b2
Kostya <ivaninvideorec@gmail.com>
2022-06-21 17:00:57 +0600

    Start working on OpenFusion. Add basic WPF rendering

---
+++ OpenFusion/Engine/Drawable.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using LearnOpenTK.Common;
+using OpenTK.Graphics.OpenGL4;
+
+namespace OpenFusion.Engine
+{
+    public class Drawable
+    {
+        public Shader _shader;
+        private int _vertexBufferObject;
+        private int _vertexArrayObject;
+        float[] _vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+            0.5f, -0.5f, 0.0f, //Bottom-right vertex
+            0.0f,  0.5f, 0.0f  //Top vertex
+        };
+
+
+
+        public Drawable(float xOffset,float yOffset)
+        {
+            /*_vertices[0] -= xOffset;
+            _vertices[3] -= xOffset;
+            _vertices[6] -= xOffset;
+            _vertices[9] -= xOffset;
+
+            _vertices[1] -= yOffset;
+            _vertices[4] -= yOffset;
+            _vertices[7] -= yOffset;
+            _vertices[10] -= yOffset;*/
+        }
+        public void Init()
+        {
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+
+
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+            
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+
+            // Enable variable 0 in the shader.
+            GL.EnableVertexAttribArray(0);
+
+
+            _shader = new Shader("shader.vert", "shader.frag");
+
+           
+            _shader.Use();
+        }
+        public void OnRender()
+        {
+            _shader.Use();
+
+            // Bind the VAO
+            GL.BindVertexArray(_vertexArrayObject);
+
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+        }
+        
+        
+    }
+}
---
+++ OpenFusion/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenFusion.Engine;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Wpf;
+
+namespace OpenFusion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var settings = new GLWpfControlSettings
+            {
+                MajorVersion = 4,
+                MinorVersion = 5
+            };
+            OpenTkControl.Start(settings);
+            while(!OpenTkControl.IsInitialized){}
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            foreach (var obj in objects)
+            {
+                obj.Init();
+            }
+            
+        }
+
+        private void OpenTkControl_OnRender(TimeSpan obj)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.ClearColor(0, 0, 0.3f, 1.0f);
+            
+            foreach (var drawable in objects)
+            {
+                drawable.OnRender();
+            }
+            
+            
+        }
+        List<Drawable> objects = new List<Drawable>()
+        {
+            new Drawable(0,0)
+        };
+
+        private void OpenTkControl_OnInitialized(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void OpenTkControl_OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Console.WriteLine($"Size changed: {e.NewSize}");
+            GL.Viewport(0, 0, 200,200);
+
+        }
+    }
+}
