dababf1a5f039462c7d79b4d84d96d1db081739e
UnknownAMD <94629779+UnknownAMD@users.noreply.github.com>
2022-07-27 15:03:25 +0300

    Make requested changes
    this took 1 hour because I was debugging a stupid error that ended up being the color of the text :skull:

--- Tools/JFusion/Program.cs
+++ Tools/JFusion/Program.cs
@@ -23,7 +23,7 @@ public class Program
     public static void Main(string[] args)
     {
         Core.Init();
-        SELECT_MODE:
+    SELECT_MODE:
         Console.ForegroundColor = ConsoleColor.DarkMagenta;
         foreach (var ascartLine in asciiArt)
         {
@@ -31,13 +31,19 @@ public class Program
         }
 
         Console.ForegroundColor = ConsoleColor.Green;
-        
-        Console.WriteLine("1. Convert MFA to Json");
-        Console.WriteLine("2. Convert Json to MFA");
+
+        Console.WriteLine("1. Convert MFA->Json");
+        Console.WriteLine("2. Convert Json-MFA");
         Console.WriteLine("3. Read and write MFA (Debug)");
         var key = Console.ReadKey().Key;
         Console.Clear();
         Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+
+        string mfapath = string.Empty;
+        string jpath = string.Empty;
+
+
         foreach (var ascartLine in asciiArt)
         {
             Console.WriteLine(ascartLine);
@@ -47,76 +53,77 @@ public class Program
             switch (key)
             {
                 case ConsoleKey.D1:
-                    MFAPATH:
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    string mfapath = string.Empty;
-                    Console.Write("MFA path: ");
-                    mfapath = Console.ReadLine().Trim('"');
-                    if (!File.Exists(mfapath))
+                    while (true)
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("ERROR: File not found");
-                        goto MFAPATH;
+                        try
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("MFA path: ");
+                            mfapath = Console.ReadLine().Trim('"');
+                            string JOutN = string.Empty;
+                            Console.WriteLine("Output Name: ");
+                            JOutN = Console.ReadLine();
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Converting MFA to Json");
+                            Console.WriteLine("Reading MFA...");
+                            var newMfa = new MFAData();
+                            newMfa.Read(new ByteReader(mfapath, FileMode.Open));
+                            Console.WriteLine("MFA reading finished.");
+                            Console.WriteLine("Converting MFA...");
+                            var mfaFProj = JMFAFile.FromMFA(newMfa);
+                            Console.WriteLine("MFA conversion finished.");
+                            Console.WriteLine("Writing Json...");
+                            mfaFProj.Write($"output\\{JOutN}");
+                            Console.WriteLine("Json writing finished");
+                        }
+                        catch when (!File.Exists(mfapath))
+                        {
+
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("ERROR: File not found");
+                            // goto MFAPATH;
+                        }
                     }
-                    string JOutN = string.Empty;
-                    Console.WriteLine("Output Name: ");
-                    JOutN = Console.ReadLine();
-                    string JOutP = string.Empty;
-                    Console.WriteLine("Output Path: ");
-                    JOutP = Console.ReadLine().TrimEnd('\\');
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Converting MFA to Json");
-                    Console.WriteLine("Reading MFA...");
-                    var newMfa = new MFAData();
-                    newMfa.Read(new ByteReader(mfapath, FileMode.Open));
-                    Console.WriteLine("MFA reading finished.");
-                    Console.WriteLine("Converting MFA...");
-                    var mfaFProj = JMFAFile.FromMFA(newMfa);
-                    Console.WriteLine("MFA conversion finished.");
-                    Console.WriteLine("Writing Json...");
-                    mfaFProj.Write($"{JOutP}\\{JOutN}");
-                    Console.WriteLine("Json writing finished");
                     break;
-                
+
 
                 case ConsoleKey.D2:
-                    JSONPATH:
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    string jpath = string.Empty;
-                    Console.Write("Json path: ");
-                    jpath = Console.ReadLine().Trim('"');
-                    if (!File.Exists(jpath))
+                    while (true)
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("ERROR: Project not found");
-                        goto JSONPATH;
+                        try
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.Write("Json path: ");
+                            jpath = Console.ReadLine().Trim('"');
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            string MOutN = string.Empty;
+                            Console.WriteLine("Output Name: ");
+                            MOutN = Console.ReadLine();
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Converting Json to MFA");
+                            Console.WriteLine("Reading Json...");
+                            var ogFProj = JMFAFile.Open(jpath);
+                            Console.WriteLine("Json reading finished.");
+                            Console.WriteLine("Converting Json...");
+                            var mfa = ogFProj.ToMFA();
+                            Console.WriteLine("Json conversion finished.");
+                            Console.WriteLine("Writing MFA...");
+                            mfa.Write(new ByteWriter($"output\\{MOutN}.mfa", FileMode.Create));
+                            Console.WriteLine("MFA writing finished");
+                        }
+                        catch when (!File.Exists(mfapath))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("ERROR: Project not found");
+                        }
                     }
-                    MFAOUT:
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    string MOutN = string.Empty;
-                    string MOutP = string.Empty;
-                    Console.WriteLine("Output Name: ");
-                    MOutN = Console.ReadLine();
-                    Console.WriteLine("Output Path: ");
-                    MOutP = Console.ReadLine().TrimEnd('\\');
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Converting Json to MFA");
-                    Console.WriteLine("Reading Json...");
-                    var ogFProj = JMFAFile.Open(jpath);
-                    Console.WriteLine("Json reading finished.");
-                    Console.WriteLine("Converting Json...");
-                    var mfa = ogFProj.ToMFA();
-                    Console.WriteLine("Json conversion finished.");
-                    Console.WriteLine("Writing MFA...");
-                    mfa.Write(new ByteWriter($"{MOutP}\\{MOutN}.mfa", FileMode.Create));
-                    Console.WriteLine("MFA writing finished");
                     break;
-                
+
 
                 case ConsoleKey.D3:
                     var testMfa = new MFAData();
-                    testMfa.Read(new ByteReader("test.mfa",FileMode.Open));
-                    testMfa.Write(new ByteWriter("test_out.mfa",FileMode.Create));
+                    testMfa.Read(new ByteReader("test.mfa", FileMode.Open));
+                    testMfa.Write(new ByteWriter("test_out.mfa", FileMode.Create));
                     break;
                 default:
                     goto SELECT_MODE;
