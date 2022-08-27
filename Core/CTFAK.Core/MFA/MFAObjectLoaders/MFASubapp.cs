901835cb66a769b8c8540b4f1763e53e57f65d89
UnknownAMD <94629779+UnknownAMD@users.noreply.github.com>
2022-08-27 14:18:43 +0300

    Start working on SubApplications

---
+++ Core/CTFAK.Core/MFA/MFAObjectLoaders/MFASubapp.cs
@@ -0,0 +1,15 @@
+﻿using CTFAK.CCN.Chunks;
+using CTFAK.Memory;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTFAK.MFA.MFAObjectLoaders;
+
+public class MFASubapp
+{
+  //  public MFASubapp(ByteReader reader) : base(reader) { }
+}
