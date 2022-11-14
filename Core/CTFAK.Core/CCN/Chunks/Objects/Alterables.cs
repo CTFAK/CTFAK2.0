6e33f45918bf03dc48f0a9f5d737f03a61111646
Yunivers <AloneInTheYunivers@gmail.com>
2022-11-14 08:01:40 -0700

    Added Alterable Values, started work on Object Effects.

---
+++ Core/CTFAK.Core/CCN/Chunks/Objects/Alterables.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using CTFAK.CCN.Chunks;
+using CTFAK.Memory;
+using CTFAK.Utils;
+
+namespace CTFAK.CCN.Chunks.Objects
+{
+    public class AlterableValues:ChunkLoader
+    {
+        public List<int> Items = new List<int>();
+
+        public override void Read(ByteReader reader)
+        {
+            var count = reader.ReadInt16();
+            for (int i = 0; i < count; i++)
+            {
+                Items.Add(reader.ReadInt32());
+                //Logger.Log($"Reading AltVal {i}: {Items[i]}");
+            }
+        }
+
+        public override void Write(ByteWriter writer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class AlterableStrings : ChunkLoader
+    {
+        public List<string> Items = new List<string>();
+
+        public override void Read(ByteReader reader)
+        {
+            var count = reader.ReadInt16();
+            for (int i = 0; i < count; i++)
+            {
+                Items.Add(reader.ReadWideString());
+                //Logger.Log($"Reading AltStr {i}: {Items[i]}");
+            }
+        }
+
+        public override void Write(ByteWriter writer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
