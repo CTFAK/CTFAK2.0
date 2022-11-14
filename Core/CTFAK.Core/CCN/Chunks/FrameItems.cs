576c7e6967f1626ac18673c117846e94be4130b4
Kostya <ivaninvideorec@gmail.com>
2022-11-14 21:49:45 +0600

    remove useless file

--- Core/CTFAK.Core/CCN/Chunks/FrameItems.cs
+++ Core/CTFAK.Core/CCN/Chunks/FrameItems.cs
@@ -11,7 +11,29 @@ using CTFAK.CCN.Chunks.Objects;
 namespace CTFAK.CCN.Chunks
 {
     [ChunkLoader(8745,"FrameItems")]
+
     public class FrameItems : ChunkLoader
+    {
+        public Dictionary<int, ObjectInfo> Items=new Dictionary<int, ObjectInfo>();
+        
+        public override void Read(ByteReader reader)
+        {
+            var count = reader.ReadInt32();
+            for (int i = 0; i < count; i++)
+            {
+                var newObject = new ObjectInfo();
+                newObject.Read(reader);
+                Items.Add(newObject.handle,newObject);
+            }
+        }
+
+        public override void Write(ByteWriter Writer)
+        {
+            
+        }
+    }
+    [ChunkLoader(8767,"FrameItems2")]
+    public class FrameItems2 : ChunkLoader
     {
         public Dictionary<int, ObjectInfo> Items=new Dictionary<int, ObjectInfo>();
         
