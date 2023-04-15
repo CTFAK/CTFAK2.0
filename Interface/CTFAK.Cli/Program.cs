ecbad42769c0fd8ed4267e43c41977557570925a
Yunivers <92553545+AITYunivers@users.noreply.github.com>
2023-04-15 12:44:36 -0700

    Added support for program arguments, fixed plugins not showing in cmd.

--- Interface/CTFAK.Cli/Program.cs
+++ Interface/CTFAK.Cli/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Threading;
 using CTFAK;
@@ -14,9 +15,17 @@ public class Program
 {
     public static IFileReader gameParser;
     public static string builddate = "4/15/23";
+    public static bool didToolArg = false;
 
     public static void Main(string[] args)
     {
+        var processModule = Process.GetCurrentProcess().MainModule;
+        if (processModule != null)
+        {
+            var pathToExe = processModule.FileName;
+            var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            Directory.SetCurrentDirectory(pathToContentRoot);
+        }
         CTFAK.CTFAKCore.Init();
         ASCIIArt.SetStatus("Idle");
         Directory.CreateDirectory("Plugins");
@@ -29,9 +38,6 @@ public class Program
         Thread.Sleep(700);
         Console.Clear();
 
-
-
-
         ASCIIArt.DrawArt();
         Console.ForegroundColor = ConsoleColor.Cyan;
         Console.WriteLine("by 1987kostya and Yunivers");
@@ -41,10 +47,26 @@ public class Program
         ASCIIArt.SetStatus("Waiting for file");
         Console.ResetColor();
         string path = string.Empty;
-        if (args.Length == 0)
+        CTFAKCore.parameters = string.Empty;
+        if (args.Length != 1)
         {
-            Console.Write("Game path: ");
-            path = Console.ReadLine().Trim('"');
+            if (!args.Contains("-path"))
+            {
+                Console.Write("Game path: ");
+                path = Console.ReadLine().Trim('"');
+            }
+            else
+            {
+                bool foundpath = false;
+                foreach (string s in args)
+                    if (foundpath)
+                    {
+                        path = s;
+                        break;
+                    }
+                    else if (s == "-path")
+                        foundpath = true;
+            }
         }
         else path = args[0];
 
@@ -53,51 +75,99 @@ public class Program
             Console.WriteLine("ERROR: File not found");
             goto ASK_FOR_PATH;
         }
-        Console.Write("Parameters: ");
-        var loadParams = Console.ReadLine();
-        CTFAK.CTFAKCore.parameters = loadParams;
+        if (args.Length != 1)
+        {
+            if (!args.Contains("-path"))
+            {
+                Console.Write("Parameters: ");
+                CTFAKCore.parameters = Console.ReadLine();
+            }
+            else
+            {
+                bool foundparams = false;
+                foreach (string s in args)
+                    if (foundparams)
+                    {
+                        CTFAKCore.parameters = s;
+                        break;
+                    }
+                    else if (s == "-parameters")
+                        foundparams = true;
+            }
+        }
 
         var types = Assembly.GetAssembly(typeof(ExeFileReader)).GetTypes();
 
         List<IFileReader> availableReaders = new List<IFileReader>();
 
 
-
-        if (Path.GetExtension(path)==".exe")
-            gameParser = new ExeFileReader();
-        else if (Path.GetExtension(path) == ".apk")
+        if (!args.Contains("-forcetype"))
         {
-            if (File.Exists(Path.GetTempPath() + "application.ccn"))
-                File.Delete(Path.GetTempPath() + "application.ccn");
-            path = ApkFileReader.ExtractCCN(path);
-            gameParser = new CCNFileReader();
+            if (Path.GetExtension(path) == ".exe")
+                gameParser = new ExeFileReader();
+            else if (Path.GetExtension(path) == ".apk")
+            {
+                if (File.Exists(Path.GetTempPath() + "application.ccn"))
+                    File.Delete(Path.GetTempPath() + "application.ccn");
+                path = ApkFileReader.ExtractCCN(path);
+                gameParser = new CCNFileReader();
+            }
+            else if (Path.GetExtension(path) == ".mfa")
+                gameParser = new MFAFileReader();
+            else
+            {
+            SELECT_READER:
+                foreach (var rawType in types)
+                    if (rawType.GetInterface(typeof(IFileReader).FullName) != null)
+                        availableReaders.Add((IFileReader)Activator.CreateInstance(rawType));
+                foreach (var item in Directory.GetFiles("Plugins", "*.dll"))
+                {
+                    var newAsm = Assembly.LoadFrom(Path.GetFullPath(item));
+                    foreach (var pluginType in newAsm.GetTypes())
+                        if (pluginType.GetInterface(typeof(IFileReader).FullName) != null)
+                            availableReaders.Add((IFileReader)Activator.CreateInstance(pluginType));
+                }
+                //Console.Clear();
+                ASCIIArt.DrawArt();
+                ASCIIArt.SetStatus("Selecting tool");
+                Console.WriteLine($"{availableReaders.Count} readers(s) available\n\nSelect reader: ");
+                Console.WriteLine("0. Exit CTFAK");
+                for (int i = 0; i < availableReaders.Count; i++)
+                    Console.WriteLine($"{i + 1}. {availableReaders[i].Name}");
+                var key1 = Console.ReadLine();
+                var readerSelect = int.Parse(key1);
+                if (readerSelect == 0) Environment.Exit(0);
+                gameParser = availableReaders[readerSelect - 1];
+            }
         }
-        else if (Path.GetExtension(path) == ".mfa")
-            gameParser = new MFAFileReader();
         else
         {
-            SELECT_READER:
-            foreach (var rawType in types)
-                if (rawType.GetInterface(typeof(IFileReader).FullName) != null)
-                    availableReaders.Add((IFileReader)Activator.CreateInstance(rawType));
-            foreach (var item in Directory.GetFiles("Plugins", "*.dll"))
-            {
-                var newAsm = Assembly.LoadFrom(Path.GetFullPath(item));
-                foreach (var pluginType in newAsm.GetTypes())
-                    if (pluginType.GetInterface(typeof(IFileReader).FullName) != null)
-                        availableReaders.Add((IFileReader)Activator.CreateInstance(pluginType));
-            }
-            //Console.Clear();
-            ASCIIArt.DrawArt();
-            ASCIIArt.SetStatus("Selecting tool");
-            Console.WriteLine($"{availableReaders.Count} readers(s) available\n\nSelect reader: ");
-            Console.WriteLine("0. Exit CTFAK");
-            for (int i = 0; i < availableReaders.Count; i++)
-                Console.WriteLine($"{i + 1}. {availableReaders[i].Name}");
-            var key1 = Console.ReadLine();
-            var readerSelect = int.Parse(key1);
-            if (readerSelect == 0) Environment.Exit(0);
-            gameParser = availableReaders[readerSelect - 1];
+            bool foundtype = false;
+            foreach (string s in args)
+                if (foundtype)
+                {
+                    switch (s)
+                    {
+                        case "exe":
+                            gameParser = new ExeFileReader();
+                            break;
+                        case "ccn":
+                            gameParser = new CCNFileReader();
+                            break;
+                        case "apk":
+                            if (File.Exists(Path.GetTempPath() + "application.ccn"))
+                                File.Delete(Path.GetTempPath() + "application.ccn");
+                            path = ApkFileReader.ExtractCCN(path);
+                            gameParser = new CCNFileReader();
+                            break;
+                        case "mfa":
+                            gameParser = new MFAFileReader();
+                            break;
+                    }
+                    break;
+                }
+                else if (s == "-forcetype")
+                    foundtype = true;
         }
             
         var readStopwatch = new Stopwatch();
@@ -130,6 +200,8 @@ public class Program
                     availableTools.Add((IFusionTool)Activator.CreateInstance(pluginType));
             }
         }
+        if (args.Contains("-closeonfinish") && !args.Contains("-tool"))
+            Environment.Exit(0);
     SELECT_TOOL:
         Console.WriteLine("");
         Console.WriteLine($"Game Information:");
@@ -139,35 +211,79 @@ public class Program
         Console.WriteLine($"Fusion Build: "+ gameParser.getGameData().productBuild);
         Console.WriteLine("");
         ASCIIArt.SetStatus("Selecting tool");
-        Console.WriteLine($"{availableTools.Count} tool(s) available\n\nSelect tool: ");
-        Console.WriteLine("0. Exit CTFAK");
-        for (int i = 0; i < availableTools.Count; i++)
+        if (!args.Contains("-tool") && !didToolArg || didToolArg)
         {
-            Console.WriteLine($"{i+1}. {availableTools[i].Name}");
+            Console.WriteLine($"{availableTools.Count} tool(s) available\n\nSelect tool: ");
+            Console.WriteLine("0. Exit CTFAK");
+            for (int i = 0; i < availableTools.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {availableTools[i].Name}");
+            }
+            var key = Console.ReadLine();
+            var toolSelect = int.Parse(key);
+            if (toolSelect == 0) Environment.Exit(0);
+            IFusionTool selectedTool = availableTools[toolSelect - 1];
+            Console.WriteLine($"Selected tool: {selectedTool.Name}. Executing");
+            var executeStopwatch = new Stopwatch();
+            executeStopwatch.Start();
+            ASCIIArt.SetStatus($"Executing {selectedTool.Name}");
+            try
+            {
+                Settings.Build = gameParser.getGameData().productBuild;
+                selectedTool.Execute(game);
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(ex);
+                Console.ReadKey();
+            }
+            executeStopwatch.Stop();
+            Console.Clear();
+            ASCIIArt.DrawArt();
+            Console.WriteLine($"Execution of {selectedTool.Name} finished in {executeStopwatch.Elapsed.TotalSeconds} seconds");
         }
-        var key = Console.ReadLine();
-        var toolSelect = int.Parse(key);
-        if (toolSelect == 0) Environment.Exit(0);
-        IFusionTool selectedTool = availableTools[toolSelect-1];
-        Console.WriteLine($"Selected tool: {selectedTool.Name}. Executing");
-        var executeStopwatch = new Stopwatch();
-        executeStopwatch.Start();
-        ASCIIArt.SetStatus($"Executing {selectedTool.Name}");
-        try
+        else
         {
-            Settings.Build = gameParser.getGameData().productBuild;
-            selectedTool.Execute(game);
+            bool foundarg = false;
+            foreach (var s in args)
+            {
+                if (foundarg)
+                {
+                    int tool = -1;
+                    for (int i = 0; i < availableTools.Count; i++)
+                        if (availableTools[i].Name == s)
+                            tool = i;
+
+                    if (tool == -1)
+                        break;
+
+                    IFusionTool selectedTool = availableTools[tool];
+                    Console.WriteLine($"Selected tool: {selectedTool.Name}. Executing");
+                    var executeStopwatch = new Stopwatch();
+                    executeStopwatch.Start();
+                    ASCIIArt.SetStatus($"Executing {selectedTool.Name}");
+                    try
+                    {
+                        Settings.Build = gameParser.getGameData().productBuild;
+                        selectedTool.Execute(game);
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.Log(ex);
+                        Console.ReadKey();
+                    }
+                    executeStopwatch.Stop();
+                    Console.Clear();
+                    ASCIIArt.DrawArt();
+                    Console.WriteLine($"Execution of {selectedTool.Name} finished in {executeStopwatch.Elapsed.TotalSeconds} seconds");
+                }
+                else if (s == "-tool")
+                    foundarg = true;
+            }
+            didToolArg = true;
         }
-        catch(Exception ex)
-        {
-            Logger.Log(ex);
-            Console.ReadKey();
-        }
-        executeStopwatch.Stop();
-        Console.Clear();
-            
-        ASCIIArt.DrawArt();
-        Console.WriteLine($"Execution of {selectedTool.Name} finished in {executeStopwatch.Elapsed.TotalSeconds} seconds");
+        if (args.Contains("-closeonfinish"))
+            Environment.Exit(0);
         goto SELECT_TOOL;
     }
 }
